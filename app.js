faff8e74f17eadb42c374e6e71d2bfbad91fb08b
Dylan Lott <lott.dylan@gmail.com>
2015-12-27 14:04:34 -0700

    starting on auth

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose'); 
+var cors = require('cors'); 
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
